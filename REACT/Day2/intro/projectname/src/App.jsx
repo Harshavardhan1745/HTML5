e98364790edc530c95c1d2e1a560ff2bd3443386
Harshavardhan <iamharsha1745email@gmail.com>
2026-07-05 15:16:00 +0530

    updated

---
+++ REACT/Day2/intro/projectname/src/App.jsx
@@ -0,0 +1,20 @@
+const App =()=>{
+
+  return(
+    <>
+     <h1>Welcome to React JS</h1>
+     <Header />
+    </>
+  )
+}
+
+export default App
+
+export const Header = () =>{
+   return(
+    <>
+     <h1>My Website Header</h1>
+    </>
+   )
+}
+

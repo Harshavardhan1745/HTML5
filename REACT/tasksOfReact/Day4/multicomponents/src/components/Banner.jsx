3953705aee010597ea5925b3087ed16e4d16770b
Harshavardhan <iamharsha1745email@gmail.com>
2026-07-11 03:19:43 +0530

    updated

--- REACT/tasksOfReact/Day4/multicomponents/src/components/Banner.jsx
+++ REACT/tasksOfReact/Day4/multicomponents/src/components/Banner.jsx
@@ -1,8 +1,12 @@
 import Logo from '../assets/images (1).jpg'
+import Mainimage from '../assets/movie_bannner.png'
 const Banner = () => {
   return (
     <>
-      <div className='flex flex-wrap'>
+    <div className='flex  justify-center pt-5'>
+      <img src={Mainimage} alt="loading"  className='w-350 h-200 rounded-md' />
+    </div>
+      <div className='flex flex-wrap justify-center items-center'>
         <div className='bg-black text-white w-fit m-5 rounded-md shadow-2xl'>
           <div className='flex justify-center items-center p-5'>
             <div className='flex flex-col justify-center items-center '>
---
+++ REACT/tasksOfReact/Day7/nonprimitive/src/App.jsx
@@ -0,0 +1,31 @@
+// const App = () => {
+//   const arr =[1,2,3,4,5,6,7,8,9,10 ]
+//   return (
+//     <>
+//     <ol className="bg-green-500 p-10 text-white"> 
+//       {arr.map((e,i)=>(
+//         <li>{e}</li>
+//       ))}
+//     </ol>
+//     <div className="bg-red-500 text-white p-10 ">
+//       {
+//        arr.filter((e,i)=><p>e%2===0</p>)
+//       }
+//     </div>
+//     </>
+//   )
+// }
+
+// export default App
+
+const App = () => {
+  const obj = {}
+  return (
+    <> 
+
+
+    </>
+  )
+}
+
+export default App

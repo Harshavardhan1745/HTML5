d768ccb5018dc971ac319aa25711466e3bb3426d
Harshavardhan <iamharsha1745email@gmail.com>
2026-07-21 00:38:29 +0530

    updated

--- REACT/tasksOfReact/Day11/rendering/src/App.jsx
+++ REACT/tasksOfReact/Day11/rendering/src/App.jsx
@@ -2,28 +2,68 @@ import { useState } from "react"
 
 const App = () => {
   
-  const [data,setData] = useState(true)
+  // const [data,setData] = useState(true)
 
-  const [arr,setArr] = useState([1,2,3,4,5,6])
+  // const [arr,setArr] = useState([1,2,3,4,5,6])
   
 
-  const [obj,setObj] = useState({name:'React'})
+  // const [obj,setObj] = useState({name:'React'})
 
-  const changename = () => {
-    setObj({...obj,name : "Node"})
-  }
+  // const changename = () => {
+  //   setObj({...obj,name : "Node"})
+  // }
   
-  const addnum = () =>{
-    setArr([...arr,100])
+  // const addnum = () =>{
+  //   setArr([...arr,100])
+  // }
+
+  // const clicktochange = () =>{
+  //   setData(!data)
+  // }
+
+
+  const [heroes,setHeroes] = useState(["Vijay", "Ajith", "Suriya"])
+
+  const Changehero = () => {
+    const updatehero = [...heroes]
+    updatehero[1] = "Sk"
+    setHeroes(updatehero)
   }
 
-  const clicktochange = () =>{
-    setData(!data)
+  const [fruits , setFruits] = useState(["Apple","Orange","Mango"]) 
+
+  const changefruit = () => {
+    const updatefruit = [...fruits]
+    updatefruit[1] = "Banana" 
+    setFruits(updatefruit)
+  }
+
+  const [course ,setCourse] = useState({name:"Harsha",course:"Fullstack"})
+
+  const changecourse = () =>{
+    setCourse({...course,course:"MERNStack"})
+  }
+
+  const [company,setCompany] = useState({Company : "Google" , Place : "Chennai"})
+  
+  const changecompany  = () => {
+    setCompany ({...company,Company:"ZOHO" , Place : "Coimbatore"})
+  }
+
+  const [student , setStudent] = useState([{id:1, Name : "Harsha" },
+    {id : 2, Name : "Pooja"}
+  ])
+
+  const updatestudent = () =>{
+     const updatestudentname = [...student]
+     updatestudentname[1].Name = "Karthiga"
+
+     setStudent(updatestudentname)
   }
   
   return (
     <>
-      <div className={data?"bg-black text-white p-10":"bg-red-500 text-white p-10 "}>
+      {/* <div className={data?"bg-black text-white p-10":"bg-red-500 text-white p-10 "}>
         <h1>Welcome to React </h1>
         <button className={data?"bg-red-500 p-2 rounded ":"bg-black p-2 rounded "} onClick={clicktochange}>change</button>
       </div>
@@ -41,7 +81,41 @@ const App = () => {
       <div className="bg-blue-300 p-2 text-white m-5 rounded">
         <h1 className="font-bold text-2xl">{obj.name}</h1>
         <button className="bg-black p-2 text-white w-40 rounded" onClick={changename}>change</button>
+      </div> */}
+
+      <div className="p-10 flex flex-col gap-5">
+        {heroes.map((e,i)=>(
+         <p key={i+1}>{e}</p>
+        ))}
+        <button className="bg-black w-40 text-white p-2 rounded" onClick={Changehero}>Change</button>
       </div>
+
+      <div className="p-10 flex flex-col gap-5">
+        {fruits.map((e,i)=>(
+         <p key={i+1}>{e}</p>
+        ))}
+        <button className="bg-black w-40 text-white p-2 rounded" onClick={changefruit}>Change</button>
+      </div>
+
+      <div className="p-10 flex flex-col gap-5">
+        <h1>Name : {course.name}</h1>
+        <h1>Course : {course.course}</h1>
+        <button className="bg-black w-40 text-white p-2 rounded" onClick={changecourse}> Updatecourse</button>
+      </div>
+
+      <div className="p-10 flex flex-col gap-5">
+        <h1>Name : {company.Company}</h1>
+        <h1>Course : {company.Place}</h1>
+        <button className="bg-black w-40 text-white p-2 rounded" onClick={changecompany}> Updatecourse</button>
+      </div>
+
+      {student.map((s)=>(
+        <div key={s.id} className="p-10 flex flex-col gap-5">
+        <h1>ID : {s.id}</h1>
+        <h1>Name : {s.Name}</h1>
+        <button className="bg-black w-40 text-white p-2 rounded" onClick={updatestudent}>Update Student</button>
+      </div>
+      ))}
     </>
   )
 }

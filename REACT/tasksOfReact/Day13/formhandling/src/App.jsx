db1bff36355cd0fec40ca01e787ec9e8c6126ac6
Harshavardhan <iamharsha1745email@gmail.com>
2026-07-21 23:55:34 +0530

    updated

--- REACT/tasksOfReact/Day13/formhandling/src/App.jsx
+++ REACT/tasksOfReact/Day13/formhandling/src/App.jsx
@@ -1,27 +1,92 @@
-import { useState } from "react"
+import { useState } from "react";
 
 const App = () => {
-   
-  const [saveData , setsaveData] = useState({username:""})
-  
-  const [Datas , setDatas ]  = useState([])
-  
-  const clicktoadd = (e) =>{
-       e.preventDefault()
-       setDatas([...Datas,saveData])
+  const [username,setUsername] = useState(" ")
+  const [savedata,setSavedata] = useState([])
+  const handlechange = (e) =>{
+    
+    setUsername(e.target.value) 
+  }
+  const showvalue = () => {
+    setSavedata(username)
+    setUsername(" ")
+  }
 
+  const [student,setStudent] = useState("")
+  const [data,setData] = useState([])
+  const [course,setcourse] = useState("")
+  const [datas,setdatas] = useState([])
+
+  const addstudent = (e) =>{
+    setStudent(e.target.value)
+  }
+  const addstudentcourse = (e) =>{
+    setcourse(e.target.value)
+  }
+  
+  const displayvalue = () =>{
+    setData(student)
+    setStudent("")
+    setdatas(course)
+    setcourse("")
+  }
+
+  const [employee,setEmployee] = useState("")
+  const [employeename,setEmployeename] = useState([])
+
+  const [department,setDepartment] = useState("")
+  const [depart,setDepart] = useState([])
+
+  const [salary,setSalary] = useState("")
+  const [amt,setAmt] = useState([])
+
+  const addemp = (e) =>{
+    setEmployee(e.target.value)
+  }
+
+  const adddepart =(e)=>{
+    setDepartment(e.target.value)
+  }
+
+  const addsalary =(e)=>{
+    setSalary(e.target.value)
+  }
+
+  const showempdetails =()=>{
+    setEmployeename(employee)
+    setEmployee("")
+    setDepart(department)
+    setDepartment("")
+    setAmt(salary)
+    setSalary("")
   }
 
   return (
     <>
-     <form onSubmit={clicktoadd}> 
-      <input type="text"  placeholder="Enter your name " onChange={(e)=>setsaveData({username:e.target.value})}/>
-      <input type="submit"/>
-     </form>
-    
     <div>
-      <h1>{Datas}</h1>
+      {/* <p>{username}</p> */}
+      <input type="text" value={username} placeholder="Enter your name" onChange={handlechange}/>
+      <button onClick={showvalue}>Add value</button>
     </div>
+    <h1>{savedata}</h1>
+
+    <div>
+      <input type="text" value={student} placeholder="Enter your name " onChange={addstudent}/>
+      <input type="text" value={course} placeholder="Enter your course " onChange={addstudentcourse}/>
+      <button onClick={displayvalue}>Show</button>
+    </div>
+    <h1>Student Name :{data}</h1>
+    <h1>Course :{datas}</h1>
+
+    <div>
+      <input type="text" placeholder="enter emp name" onChange={addemp}/>
+      <input type="text" placeholder="enter emp department" onChange={adddepart}/>
+      <input type="number" placeholder="enter emp salary " onChange={addsalary}/>
+      <button onClick={showempdetails}>Show data</button>
+    </div>
+    <h1>Employee Name : {employeename}</h1>
+    <h1>Department : {depart}</h1>
+    <h1>Salary : {amt}</h1>
     </>
   )
 }

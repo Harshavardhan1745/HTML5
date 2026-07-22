911020d82c83b43c81afbf57504d362bbd59df97
Harshavardhan <iamharsha1745email@gmail.com>
2026-07-23 00:06:23 +0530

    updated

--- REACT/tasksOfReact/Day14/useeffect/src/App.jsx
+++ REACT/tasksOfReact/Day14/useeffect/src/App.jsx
@@ -1,10 +1,32 @@
+import { useState } from "react"
+
 const App = () => {
-  
-  const 
+  const [adddata,setAddData] = useState("")
+  const [data,setData] = useState([])
+
+  const adddatas = (e) => {
+    setAddData(e.target.value)
+  }
+
+  const showaddvalue =()=>{
+    setData(adddata)
+    setAddData("")
+  }
+
   
   return (
     <>
-    
+    <div>
+      <input type="text" value={adddata} placeholder="Enter your name" onChange={adddatas}/>
+      <button className="" onClick={showaddvalue}>Show value</button>
+      <h1>{data}</h1>
+    </div>
+   
+   <div>
+    <input type="text" placeholder="Enter your name " onChange={addstudentname}/>
+    <input type="text" placeholder="Enter your course " onChange={addstudentcourse}/>
+    <button></button>
+   </div>
     </>
   )
 }
